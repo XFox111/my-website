15b69d7e425ba903e3958fda5b92678c66a59f9c
Eugene Fox <eugene@xfox111.net>
2024-08-21 00:25:00 +0000

    style: minor styling updates

--- public/clarity.js
+++ public/clarity.js
@@ -9,12 +9,14 @@
  * Original code snippet:
  *
  * <script type="text/javascript">
- * 		(function(c,l,a,r,i,t,y){
- * 			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
- * 			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
- * 			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
- * 		})(window, document, "clarity", "script", "[redacted_clarity_id]");
- * 	</script>
+ * 	(function(c,l,a,r,i,t,y){
+ * 		c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+ * 		t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+ * 		y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+ * 	})(window, document, "clarity", "script", "[redacted_clarity_id]");
+ * </script>
+ *
+ * All Clarity tracking code is available on https://github.com/microsoft/clarity
  */
 
 // If you want to setup Clarity analytics as well, you'll need to specify the ID in .env file,
