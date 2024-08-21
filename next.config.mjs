dceb2e44b7f5dbfc9f802bfd519c537ad70b4cf6
Eugene Fox <eugene@xfox111.net>
2024-08-21 00:19:09 +0000

    fix: added CSP policy for Clarity

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,21 @@ const nextConfig = {
 	output: "standalone",
 	reactStrictMode: true,
 	productionBrowserSourceMaps: true,
+	async headers()
+	{
+		return [
+			{
+				source: "/(.*)",
+				headers:
+				[
+					{
+						key: "Content-Security-Policy",
+						value: "default-src 'self' https://*.clarity.ms https://c.bing.com 'unsafe-inline';"
+					}
+				]
+			}
+		];
+	}
 };
 
 export default nextConfig;
